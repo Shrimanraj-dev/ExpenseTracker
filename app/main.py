08fa379da703df33fa6e63b8c93477fd7e3b6e62
Shrimanraj L J <shrimanraj.lj@gmail.com>
2025-07-14 21:03:54 +0530

    added

--- app/main.py
+++ app/main.py
@@ -2,10 +2,18 @@ from flask import Flask, request, render_template
 import boto3
 from datetime import datetime
 import uuid
+import logging
+from decimal import Decimal
 
 app = Flask(__name__)
 dynamodb = boto3.resource('dynamodb',region_name='ap-south-1')
 table = dynamodb.Table('Expenses')
+
+logging.basicConfig(
+    filename='/var/log/expense-tracker.log',   # Log file path
+    level=logging.INFO,                        # Log level
+    format='%(asctime)s [%(levelname)s] %(message)s'
+)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -13,7 +21,7 @@ def index():
         item = {
             'id': str(uuid.uuid4()),
             'category': request.form['category'],
-            'amount': float(request.form['amount']),
+            'amount': Decimal(request.form['amount']),
             'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         }
         table.put_item(Item=item)
